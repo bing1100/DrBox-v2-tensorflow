7beab7dc5cd7f95c9c5928593f9d93b077806362
bing1100 <bingxuhu@gmail.com>
2020-08-06 16:47:43 -0400

    added comments to file

--- imagesplit.py
+++ imagesplit.py
@@ -1,14 +1,16 @@
 import cv2
 from pathos.multiprocessing import ProcessingPool as Pool
 
-processNumbers = 4
-split_width = 300
-split_height = 300
-overlap = 0.5
+# Note: Copy this file to the train directory before running. 
 
-def start_points(size, split_size, overlap=0):
+NUMPROCESS = 4
+NEWWIDTH = 300
+NEWHEIGHT = 300
+OVERLAP = 0.5
+
+def start_points(size, split_size):
     points = [0]
-    stride = int(split_size * (1-overlap))
+    stride = int(split_size * (1-OVERLAP))
     counter = 1
     while True:
         pt = stride * counter
@@ -23,10 +25,9 @@ def start_points(size, split_size, overlap=0):
 def checkKeyPoints(yStart, xStart, ys, xs):
     for x in xs:
         for y in ys:
-            if x < xStart or (xStart + split_width) < x:
+            if x < xStart or (xStart + NEWWIDTH) < x:
                 return False
-
-            if y < yStart or (yStart + split_height) < y:
+            if y < yStart or (yStart + NEWHEIGHT) < y:
                 return False
     return True
             
@@ -43,35 +44,42 @@ def processImg(num):
     kps = [line.split(",") for line in lines[:-1]]
 
     # Find Starting Points to split the image
-    X_points = start_points(img_w, split_width, overlap)
-    Y_points = start_points(img_h, split_height, overlap)
+    X_points = start_points(img_w, NEWWIDTH, OVERLAP)
+    Y_points = start_points(img_h, NEWHEIGHT, OVERLAP)
 
-    count = 0
     name = str(num)
     frmt = 'tif'
+
+    # Iterate over all images that can be split
+    count = 0
     for i in Y_points:
         for j in X_points:
-            
-            count += 1
-
-            kpCount = 0
             newLines = []
+
+            # Iterate over all keypoints that could be on split image
+            kpCount = 0
             for kp in kps:
                 xs = [int(float(i)) for i in kp[0::2]]
                 ys = [int(float(i)) for i in kp[1::2]]
-
+                
+                # Check if the keypoints are on this image
                 if checkKeyPoints(i, j, ys, xs):
+                    # Add keypoints on this image to be added to the kp file
                     newLines.append(lines[kpCount] + "\n")
-                    split = img[i:i+split_height, j:j+split_width]
+                    # Generate and save the split image
+                    split = img[i:i+NEWHEIGHT, j:j+NEWWIDTH]
                     cv2.imwrite('./splitImages/{}_{}.{}'.format(name, count, frmt), split)
                 
                 kpCount += 1
             
+            # Save the kps for the split image only if there are kps
             if len(newLines) != 0:
                 with open('./splitKeyPoints/{}_{}.txt'.format(name, count),'w') as target:
                     target.writelines(newLines)
+
+            count += 1
     
-p = Pool(processNumbers)
+p = Pool(NUMPROCESS)
 p.map(processImg, list(range(1,1001)))
 
 
